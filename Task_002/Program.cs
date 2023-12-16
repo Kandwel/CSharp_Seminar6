8ccad2e61112ccbab3096ee294e5d131c5c7736d
unknown <C:\Users\GMS\AppData\Roaming\The Bat!>
2023-12-16 15:51:14 +0700

    Задание 2

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -2,3 +2,15 @@
 //Задайте строку, содержащую латинские буквы
 //в обоих регистрах. Сформируйте строку, в которой 
 //все заглавные буквы заменены на строчные.
+
+Console.WriteLine("Программа превращает всё в строчные буквы");
+main();
+
+void main()
+{
+    string input = "HoMeWorK gEEkbRaIns!-";
+
+    string result = input.ToLower();
+
+    Console.WriteLine(input + " => " + result);
+}
