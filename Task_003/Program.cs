7b488f3321505d9ff7864c039f377f6f0583c835
unknown <C:\Users\GMS\AppData\Roaming\The Bat!>
2023-12-16 15:58:26 +0700

    Задание 3

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -1,3 +1,19 @@
 ﻿//Задача 3:
 //Задайте произвольную строку. 
 //Выясните, является ли она палиндромом.
+
+
+Console.WriteLine("Программа проверяет, является ли введённое значение палидромом");
+System.Console.WriteLine();
+
+System.Console.WriteLine("Введите символы");
+string input = Console.ReadLine();
+bool isPalindrome = IsPalindrome(input);
+Console.WriteLine(isPalindrome ? $"Ваши символы '{input}' палидром" : $"Ваши символы '{input}' не палидром");
+
+bool IsPalindrome(string str)
+{
+    string normalized = new
+    string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    return normalized.SequenceEqual(normalized.Reverse());
+}
