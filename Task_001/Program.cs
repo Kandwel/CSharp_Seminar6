84795116bb174c1b99f5650ca627fef37979a2ad
unknown <C:\Users\GMS\AppData\Roaming\The Bat!>
2023-12-16 15:46:20 +0700

    Задание 1

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -2,3 +2,22 @@
 //Задайте двумерный массив символов (тип char[,]). 
 //Создать строку из символов этого массива. 
 
+Console.WriteLine("Программа показывает весь массив в одной строке");
+
+char[,] charArray = new char[,] { { 'H', 'W' }, { 'G', 'B' } };
+
+string result = CreateStringFrom(charArray);
+Console.WriteLine(result);
+
+static string CreateStringFrom(char[,] array)
+{
+    string result = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result += array[i, j];
+        }
+    }
+    return result;
+}
